b664b9760d78855cdc3a67571ad0d24dffeba342
Jeyphr <123409792+Jeyphr@users.noreply.github.com>
2024-12-29 17:20:59 -0500

    added NME Scripts
    egg

--- Assets/Prefabs/NME/NME Movement.cs
+++ Assets/Prefabs/NME/NME Movement.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NMEMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Object References")]
+    [SerializeField] private NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Statistics")]
+    [SerializeField] private Vector3 position;
+
+    //refs
+    private Transform goal;
+
+    //vars
+    const float updateSpeed = 0.1f;
+
+
+    #region Generic
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Start()
     {
         
     }
+    #endregion
+
+    #region Navigation Methods
+    private IEnumerator FollowGoal()
+    {
+        WaitForSeconds _wait = new WaitForSeconds(updateSpeed);
+        while (enabled)
+        {
+            agent.SetDestination(goal.position);
+            yield return null;
+        }
+    }
+    #endregion
 }
